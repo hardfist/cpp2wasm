c9509d42f986aa6880e8afb67e69261b0e7658e2
hardfist <1562502418@qq.com>
2020-06-18 20:04:17 +0800

    feat: add ref-napi

--- examples/addon/binding.gyp
+++ examples/addon/binding.gyp
@@ -6,6 +6,9 @@
       "include_dirs": [
         "<!(node -e \"require('nan')\")"
       ]
+    },
+    {
+      
     }
   ]
 }
--- examples/addon/index.js
+++ examples/addon/index.js
@@ -1,3 +1,7 @@
-var addon = require('bindings')('hello');
-
+const ffi = require('ffi-napi');
+const addon = require('bindings')('hello');
+var libfactorial = ffi.Library(`${__dirname}/libfactorial`, {
+  'factorial': [ 'uint64', [ 'int' ] ]
+})
+console.log(libfactorial.factorial(10))
 console.log(addon.hello()); // 'world'
